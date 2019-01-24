91ab8d0d341e94e7e3c0310b3d329aa76e567791
Jihee Lee <jiheelee.ljh@gmail.com>
2019-01-24 01:01:17 +0000

    workshop12

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route("/device")
+def device():
+    return render_template("device.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
